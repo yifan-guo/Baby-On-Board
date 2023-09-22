e08cc73d6170705d64ba777e72eb5a23c7b50b14
Brian Tsai <bt92130@gmail.com>
2023-09-21 22:51:17 -0400

    feat: #18 collectible and package classes, player package tracking

--- NimbleRabbit/Assets/Scripts/Player/PlayerController.cs
+++ NimbleRabbit/Assets/Scripts/Player/PlayerController.cs
@@ -4,42 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-    public GameObject vehicle;
+    /// <summary>
+    /// Static instance of player.
+    /// </summary>
+    public static PlayerController instance {get; private set;}
 
-    public Transform Right;
-    public Transform Left;
-    public Transform Straight;
+    /// <summary>
+    /// Packages that player has collected.
+    /// </summary>
+    private List<Package> packages;
 
+    /// <summary>
+    /// Reference to Rigidbody.
+    /// </summary>
+    private Rigidbody rb;
 
-    public float forwardSpeed = 150f;
-    public float backwardsSpeed = 80f;
-    public float turnSpeed = 15f;
+    [Header("Driving")]
+    public float forwardSpeed;
+    public float backwardsSpeed;
+    public float turnSpeed;
 
-    private void Start()
+    /// <summary>
+    /// Initialization Pt I.
+    /// </summary>
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }    
+
+    /// <summary>
+    /// Initialization Pt II.
+    /// </summary>
+    private void Start()
+    {
+        instance = this;
+        packages = new List<Package>();
     }
 
+    /// <summary>
+    /// Every frame update loop.
+    /// </summary>
     private void Update()
     {
-        print(rb.velocity);
         HandleInput();
-
     }
 
+    /// <summary>
+    /// Interprets user input.
+    /// </summary>
     private void HandleInput()
     {
         if (Input.GetKey("w"))
         {
             rb.velocity += transform.forward * forwardSpeed * Time.deltaTime;
-
         }
 
         if (Input.GetKey("s"))
         {
             rb.velocity -= transform.forward * backwardsSpeed * Time.deltaTime;
-
         }
 
         // Turning left
@@ -53,5 +75,44 @@ public class PlayerController : MonoBehaviour
         {
             transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
         }
+
+        // TODO:
+        // This is test code to drop the package.
+        // Ideally DropPackage() is called elsewhere by something like an enemy.
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            packages.Count > 0)
+        {
+            DropPackage(packages[0]);
+        }
+    }
+
+    /// <summary>
+    /// Add package.
+    /// </summary>
+    /// <param name="pkg"></param>
+    public void CollectPackage(Package pkg)
+    {
+        if (packages.Contains(pkg))
+        {
+            return;
+        }
+
+        packages.Add(pkg);
+        pkg.Collect();
+    }
+
+    /// <summary>
+    /// Drop a package.
+    /// </summary>
+    /// <param name="pkg"></param>
+    public void DropPackage(Package pkg)
+    {
+        if (packages.Contains(pkg) == false)
+        {
+            return;
+        }
+
+        packages.Remove(pkg);
+        pkg.Drop();
     }
 }
