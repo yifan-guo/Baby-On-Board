53840afabd7526aa2776e9406588d4722656bd00
Yifan Guo <yifan.guo@vanderbilt.edu>
2023-11-10 17:41:32 -0600

    police cs includes EngineStateFailure and goes into cooldown after arresting player

--- NimbleRabbit/Assets/Scripts/NPC/Police.cs
+++ NimbleRabbit/Assets/Scripts/NPC/Police.cs
@@ -7,11 +7,8 @@ using UnityEngine;
 public class Police : NPC
 {
     private float ARREST_TIME = 5.0f;
-    private float SLEEP_TIME = 10.0f;
     
-    private float SPEED_LIMIT = 35.0f;
-
-    private bool PULLED_PLAYER_OVER = false;
+    private float SPEED_LIMIT = 15.0f;
 
     protected override void Awake() {
         base.Awake();
@@ -25,7 +22,8 @@ public class Police : NPC
         {
             {typeof(IdleState), new IdleState(this)},
             {typeof(ChaseState), new ChaseState(this)},
-            {typeof(AttackState), new AttackState(this)}
+            {typeof(AttackState), new AttackState(this)},
+            {typeof(EngineFailureState), new EngineFailureState(this)}
         };
 
         stateMachine.SetStates(states);
@@ -52,10 +50,6 @@ public class Police : NPC
     public override bool Chase()
     {
         // do not chase if player is under speed limit
-        if (PULLED_PLAYER_OVER) {
-            return false;
-        }
-
         if (PlayerController.instance.rb.velocity.magnitude < SPEED_LIMIT) 
         {
             return false;
@@ -74,43 +68,23 @@ public class Police : NPC
 
     public override void Attack()
     {
-        if (PULLED_PLAYER_OVER) {
-            return;
-        }
         // unity is a single-threaded application
         // sleeping on the main thread will freeze the game
         // Coroutines are not threads. They run on the main thread
-        StartCoroutine(FreezePlayer(ARREST_TIME));
+        Debug.Log("Start Couroutine");
+        StartCoroutine(FreezePlayer());
 
         // police should rest before making another arrest
-        StartCoroutine(WaitForTime(this.rb, SLEEP_TIME));
+        inCooldown = true;
     }
 
-    IEnumerator FreezePlayer(float sleep_time) 
+    IEnumerator FreezePlayer() 
     {   
-         PlayerController.instance.enabled = false;
+        Debug.Log("freeze player");
+        PlayerController.instance.rb.constraints = RigidbodyConstraints.FreezePosition;
 
-         yield return new WaitForSeconds(sleep_time);
+         yield return new WaitForSeconds(ARREST_TIME);
 
-         PlayerController.instance.enabled = true;
-    }
-
-    /// <summary>
-    /// Coroutine to suspend GameObject movement while ARRESTED
-    /// </summary>
-    IEnumerator WaitForTime(Rigidbody rb, float sleep_time)
-    {
-        // When police pulls player over, make the police stop driving around
-        PULLED_PLAYER_OVER = true;
-        this.nav.enabled = false;
-        rb.constraints = RigidbodyConstraints.FreezePosition;
-
-        // yield on a new YieldInstruction that waits for ARREST_TIME seconds
-        yield return new WaitForSeconds(sleep_time);
-
-        // After police wakes up, let it be able to arrest player and move around again
-        PULLED_PLAYER_OVER = false;
-        this.nav.enabled = true;
-        rb.constraints = RigidbodyConstraints.None;
+        PlayerController.instance.rb.constraints = RigidbodyConstraints.None;
     }
 }
