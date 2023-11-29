789665fff592ff0983a507557bf6bf1c18d169bd
Yifan Guo <yifan.guo@vanderbilt.edu>
2023-11-29 17:38:47 -0600

    make bubble show based on fixed distance of 200

--- NimbleRabbit/Assets/Scripts/Utils/SpeechBubble.cs
+++ NimbleRabbit/Assets/Scripts/Utils/SpeechBubble.cs
@@ -24,6 +24,10 @@ public class SpeechBubble : MonoBehaviour
 	private int centerOffsetX;
 	private int centerOffsetY;
 
+	private float speechBubbleVisibleRange = 200f;
+
+	private float distanceFromNPCToPlayer;
+
 	//a material to render the triangular part of the speech balloon
 	public Material mat;
 	//a guiSkin, to render the round part of the speech balloon
@@ -59,6 +63,8 @@ public class SpeechBubble : MonoBehaviour
 		//Calculate the X and Y offsets to center the speech balloon exactly on the center of the game object
 		centerOffsetX = bubbleWidth/2;
 		centerOffsetY = bubbleHeight/2;
+
+		distanceFromNPCToPlayer = Vector3.Distance(npc.transform.position, PlayerController.instance.transform.position);
 	}
 
 	//Called once per frame, after the update
@@ -76,45 +82,56 @@ public class SpeechBubble : MonoBehaviour
 	//Draw GUIs
 	void OnGUI()
 	{
-		string currentNpcStatus;
-		if (!npc.npcStatusTexts.TryGetValue(npc.stateMachine.currentState.GetType(), out currentNpcStatus)) {
-			Debug.Log("no current NPC status");
-		} else {
-		//Begin the GUI group centering the speech bubble at the same position of this game object. After that, apply the offset
-		GUI.BeginGroup(new Rect(goScreenPos.x-centerOffsetX-offsetX,Screen.height-goScreenPos.y-centerOffsetY-offsetY,bubbleWidth,bubbleHeight));
+		
+		distanceFromNPCToPlayer = Vector3.Distance(npc.transform.position, PlayerController.instance.transform.position);
 
-			//Render the round part of the bubble
-			GUI.Label(new Rect(0,0,200,100),"",guiSkin.customStyles[0]);
+		if (distanceFromNPCToPlayer < speechBubbleVisibleRange) {
+			string currentNpcStatus;
+			if (!npc.npcStatusTexts.TryGetValue(npc.stateMachine.currentState.GetType(), out currentNpcStatus)) {
+				Debug.Log("no current NPC status");
+			} else {
+			//Begin the GUI group centering the speech bubble at the same position of this game object. After that, apply the offset
+			GUI.BeginGroup(new Rect(goScreenPos.x-centerOffsetX-offsetX,Screen.height-goScreenPos.y-centerOffsetY-offsetY,bubbleWidth,bubbleHeight));
 
-			//Render the text
-			GUI.Label(new Rect(10,25,190,50), $"My status: {currentNpcStatus}", guiSkin.label);
+				//Render the round part of the bubble
+				GUI.Label(new Rect(0,0,200,100),"",guiSkin.customStyles[0]);
 
-		GUI.EndGroup();
+				//Render the text
+				GUI.Label(new Rect(10,25,190,50), $"My status: {currentNpcStatus}", guiSkin.label);
+
+			GUI.EndGroup();
+			}
 		}
+	
 	}
 
 	//Called after camera has finished rendering the scene
 	void OnRenderObject()
 	{
-		//push current matrix into the matrix stack
-		GL.PushMatrix();
-		//set material pass
-		mat.SetPass(0);
-		//load orthogonal projection matrix
-		GL.LoadOrtho();
-		//a triangle primitive is going to be rendered
-		GL.Begin(GL.TRIANGLES);
+		distanceFromNPCToPlayer = Vector3.Distance(npc.transform.position, PlayerController.instance.transform.position);
 
-			//set the color
-			GL.Color(Color.white);
+		if (distanceFromNPCToPlayer < speechBubbleVisibleRange) {
+				//push current matrix into the matrix stack
+			GL.PushMatrix();
+			//set material pass
+			mat.SetPass(0);
+			//load orthogonal projection matrix
+			GL.LoadOrtho();
+			//a triangle primitive is going to be rendered
+			GL.Begin(GL.TRIANGLES);
 
-			//Define the triangle vetices
-			GL.Vertex3(goViewportPos.x, goViewportPos.y+(offsetY/3)/Screen.height, 0.1f);
-			GL.Vertex3(goViewportPos.x - (bubbleWidth/3)/(float)Screen.width, goViewportPos.y+offsetY/Screen.height, 0.1f);
-			GL.Vertex3(goViewportPos.x - (bubbleWidth/8)/(float)Screen.width, goViewportPos.y+offsetY/Screen.height, 0.1f);
+				//set the color
+				GL.Color(Color.white);
 
-		GL.End();
-		//pop the orthogonal matrix from the stack
-		GL.PopMatrix();
+				//Define the triangle vetices
+				GL.Vertex3(goViewportPos.x, goViewportPos.y+(offsetY/3)/Screen.height, 0.1f);
+				GL.Vertex3(goViewportPos.x - (bubbleWidth/3)/(float)Screen.width, goViewportPos.y+offsetY/Screen.height, 0.1f);
+				GL.Vertex3(goViewportPos.x - (bubbleWidth/8)/(float)Screen.width, goViewportPos.y+offsetY/Screen.height, 0.1f);
+
+			GL.End();
+			//pop the orthogonal matrix from the stack
+			GL.PopMatrix();
+		}
+		
 	}
 }
